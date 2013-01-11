0892fa30b7f3ca28682044cec2b7ebd181bbc55b
Ben J. Woodcroft <donttrustben near gmail.com>
2013-01-11 13:09:42 +1000

    small doco changes

--- bin/img_metadata_scanner.rb
+++ bin/img_metadata_scanner.rb
@@ -16,39 +16,39 @@ options = {
 o = OptionParser.new do |opts|
   opts.banner = "
       Usage: #{SCRIPT_NAME} [-i <img_metadata_file>] <key=value>
-      
+
       Runs through an IMG metadata file, and prints out the IMG identifiers of those genomes that match the key=value criteria. E.g. Kingdom=Archaea to grep for all archaeons.
 
-      Example: 
+      Example:
+
 
 $ img_metadata_scanner.rb Domain=Archaea --output-fields \"Genus,Species\" |head
+
 DEBUG img_metadata_scanner: Using environment variable IMG_METADATA_FILE to define path to IMG metadata file /srv/whitlam/bio/db/img/4.0/metadata/img_metadata_4_0_FIXED.csv
  INFO img_metadata_scanner: Using 1 filters.
  INFO img_metadata_scanner: Found 4700 taxons in the IMG metadata file
 Thermococcus	gammatolerans
-Methanolobus	
-Pyrobaculum	
+Methanolobus
+Pyrobaculum
 Methanobacterium	sp.
 Sulfolobus	islandicus
 Desulfurococcus	mucosus
 Haladaptatus	paucihalophilus
 Methanothermobacter	thermautotrophicus
 Methanosarcina	barkeri
-Methanobrevibacter	smithii
-
-\n\n"
+Methanobrevibacter	smithii\n\n"
 
   opts.separator "\nOptions:\n\n"
-  opts.on("-i", "--img-metadata-file PATH", "Path to IMG metadata file [required]. This is not necessary if there is a valid environment variable #{IMG_METADATA_FILE_ENV_VARIABLE_NAME} available.") do |arg|
-    options[:img_metadata_file] = arg
-  end
   opts.on("-o", "--output-fields FIELDS", "List of output fields, comma separated [default: #{options[:output_fields].join(',')}]") do |arg|
     options[:output_fields] = arg.split ','
   end
   opts.on("-l", "--list", "Instead of filtering print a list of the fields in the output file, newline separated [default: #{options[:listing_mode]}]") do |arg|
     options[:listing_mode] = true
   end
-
+  opts.on("-i", "--img-metadata-file PATH", "Path to IMG metadata file [required]. This is not necessary if there is a valid environment variable #{IMG_METADATA_FILE_ENV_VARIABLE_NAME} available.") do |arg|
+    options[:img_metadata_file] = arg
+  end
+  
   # logger options
   opts.separator "\nVerbosity:\n\n"
   opts.on("-q", "--quiet", "Run quietly, set logging to ERROR level [default INFO]") {Bio::Log::CLI.trace('error')}
@@ -59,7 +59,7 @@ end; o.parse!
 # Setup logging. bio-logger defaults to STDERR not STDOUT, I disagree
 Bio::Log::CLI.logger(options[:logger]); log = Bio::Log::LoggerPlus.new(LOG_NAME); Bio::Log::CLI.configure(LOG_NAME)
 
-# Read in the system-specific ENV variable for the path to the img metadata file unless a path has already been specified 
+# Read in the system-specific ENV variable for the path to the img metadata file unless a path has already been specified
 if options[:img_metadata_file].nil?
   if ENV[IMG_METADATA_FILE_ENV_VARIABLE_NAME].nil?
     $stderr.puts "img-metadata-file not found as either a command line option or environment variable, failing."
@@ -75,21 +75,19 @@ unless File.exist?(options[:img_metadata_file])
   exit 2
 end
 
-
-
 # If listing mode is set, don't do any filtering, just list the variables
 if options[:listing_mode]
   # Read the IMG metadata
   img = Bio::IMG::TaxonomyDefinitionFile.read(options[:img_metadata_file])
   log.info "Found #{img.length} taxons in the IMG metadata file"
-  
+
   img[0].attributes.keys.each do |key|
     puts key
   end
-  
+
   exit 0
 end
-  
+
 # Parse the key/value pairs
 filter_hash = {}
 unless ARGV[0].nil?
@@ -99,7 +97,7 @@ unless ARGV[0].nil?
       log.error "Badly parsed key/value pair '#{split}', expected exactly 1 = sign, found #{splits2.length}"
       exit 1
     end
-    
+
     key = splits2[0]
     unless filter_hash[key].nil?
       log.error "Duplicate filter key found: #{key}, failing"
@@ -128,13 +126,13 @@ img.each do |taxon|
   passed = true
   filter_hash.each do |key, value|
     if taxon.attributes[key] != value
-      passed = false
-      break
+    passed = false
+    break
     end
   end
-  
+
   if passed
     puts options[:output_fields].collect{|field| taxon.attributes[field]}.join("\t")
   end
 end
-  
+
